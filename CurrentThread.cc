cabc311d65bcb8488ee6fe21548840a1dcc3b241
Marcus <119729684+xykCs@users.noreply.github.com>
2025-04-13 15:30:37 +0800

    init

--- CurrentThread.cc
+++ CurrentThread.cc
@@ -5,7 +5,6 @@ namespace CurrentThread{
 
     void cacheTid(){
         if(t_cachedTid == 0){
-            // 通过linux系统调用，获取当前线程的tid值
             t_cachedTid = static_cast<pid_t>(::syscall(SYS_gettid));
         }
     }
