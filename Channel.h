30aac9300d34aa73db4638e8d203c1575c6a1347
Marcus <119729684+xykCs@users.noreply.github.com>
2025-04-13 15:30:27 +0800

    Remove redundant comments

--- Channel.h
+++ Channel.h
@@ -9,9 +9,8 @@
 class EventLoop;
 
 /*
-理清楚：EventLoop、Channel、Poller之间的关系 =》 Reactor模型上对应Demultiplex
-Channel理解为通道，封装了sockfd和其感兴趣的event(如EPOLLIN、EPOLLOUT)
-还绑定了poller返回的具体事件
+EventLoop、Channel、Poller =》 Reactor::Demultiplex
+Channel : sockfd + event(EPOLLIN、EPOLLOUT) + revent
 */
 class Channel : noncopyable {
 public:
@@ -21,30 +20,25 @@ public:
     Channel(EventLoop *loop, int fd);
     ~Channel();
 
-    // fd得到poller通知以后，处理事件(调用相应的回调方法)
     void handleEvent(Timestamp receiveTime);
   
-    // 设置回调函数对象
     void setReadCallback(ReadEventCallback cb) { readCallback_ = std::move(cb); }
     void setWriteCallback(EventCallback cb) { writeCallback_ = std::move(cb); }
     void setCloseCallback(EventCallback cb) { closeCallback_ = std::move(cb); }
     void setErrorCallback(EventCallback cb) { errorCallback_ = std::move(cb); }
 
-    // 防止 当channel被手动remove掉，channel还在执行回调操作
     void tie(const std::shared_ptr<void> &);
 
     int fd() const { return fd_; }
     int events() const{return events_;}
     void set_revents(int revt) { revents_ = revt; }
     
-    // 设置fd相应的事件状态
     void enableReading() { events_ |= kReadEvent; update(); } 
     void disableReading() { events_ &= ~kReadEvent; update(); }
     void enableWriting() { events_ |= kWriteEvent; update(); }
     void disableWriting() { events_ &= ~kWriteEvent; update(); }
     void disableAll() { events_ = kNoneEvent; update(); }
 
-    // 返回fd当前的事件状态
     bool isNoneEvent() const { return events_ == kNoneEvent; }
     bool isWriting() const { return events_ & kWriteEvent; }
     bool isReading() const { return events_ & kReadEvent; }
@@ -64,18 +58,16 @@ private:
     static const int kReadEvent;
     static const int kWriteEvent;
 
-    EventLoop *loop_; // 事件循环
-    const int fd_; // 封装的sockfd,也是Poller监听的对象
-    int events_; // 注册fd感兴趣的事件
-    int revents_; // poller返回的具体发生的事件
+    EventLoop *loop_;
+    const int fd_;
+    int events_;
+    int revents_;
     
     int index_;
 
     std::weak_ptr<void> tie_;
     bool tied_;
     
-    // 因为Channel里面能够获知fd最终发生的具体的事件revents
-    // 所以Channel负责调用具体事件的回调操作
     ReadEventCallback readCallback_;
     EventCallback writeCallback_;
     EventCallback closeCallback_;
