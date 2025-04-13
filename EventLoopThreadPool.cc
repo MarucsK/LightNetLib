60fe24328a546ff6e67b1f01da59fa4fb0484822
Marcus <119729684+xykCs@users.noreply.github.com>
2025-04-13 15:35:31 +0800

    init

--- EventLoopThreadPool.cc
+++ EventLoopThreadPool.cc
@@ -20,13 +20,12 @@ void EventLoopThreadPoll::start(const ThreadInitCallback &cb){
         threads_.push_back(std::unique_ptr<EventLoopThread>(t));
         loops_.push_back(t->startLoop()); // 底层创建线程,绑定一个新的EventLoop,并返回该loop的地址
     } 
-    // 整个服务端只有一个线程运行着baseLoop_
     if(numThreads_==0 && cb){
         cb(baseLoop_);
     }
 }
 
-// 如果工作在多线程中,baseLoop_默认以轮询的方式 分配channel给subloop
+// baseLoop_默认以轮询的方式 分配channel给subloop
 EventLoop *EventLoopThreadPoll::getNextLoop(){
     EventLoop *loop = baseLoop_;
     if(!loops_.empty()){ // 通过轮询获取下一个处理事件的subloop
