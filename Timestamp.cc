b00e3006d0857840f305225002aefe4db323e15f
Marcus <119729684+xykCs@users.noreply.github.com>
2025-04-13 15:41:22 +0800

    Add comments

--- Timestamp.cc
+++ Timestamp.cc
@@ -7,15 +7,12 @@ Timestamp::Timestamp() : microSecondsSinceEpoch_(0) {}
 Timestamp::Timestamp(int64_t microSecondsSinceEpoch)
     :microSecondsSinceEpoch_(microSecondsSinceEpoch){}
 
-// 返回当前时间的Timestamp对象
 Timestamp Timestamp::now(){
-    // time(NULL)返回 时间戳（自Unix纪元到当前时间的秒数）
     return Timestamp(time(NULL)); // time_t -> int64_t 安全
 }
 
 std::string Timestamp::toString() const{
     char buf[128] = {0};
-    // 将存储的时间戳 转为tm结构
     tm *tm_time = localtime(&microSecondsSinceEpoch_);
     snprintf(buf, 128, "%4d/%02d/%02d %02d:%02d:%02d",
              tm_time->tm_year + 1900,
